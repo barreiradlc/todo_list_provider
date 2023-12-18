43614a31ee5642d36f3dfbe8e0c2f012241efb60
Augusto Barreira <barreira266@hotmail.com>
2023-12-18 10:40:05 -0300

    Login page init

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
 import 'package:todo_list_provider/app/modules/auth/login/auth_module.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
@@ -28,7 +29,10 @@ class _AppWidgetState extends State<AppWidget> {
     return MaterialApp(
       title: "Todo list provider",
       initialRoute: '/login',
-      routes: {...AuthModule().routers},
+      theme: TodoListUiConfig.theme,
+      routes: {
+        ...AuthModule().routers,
+      },
       home: SplashPage(),
     );
   }
